b930bfac01f901e64b1bafbbddfe44fd9d74b169
SydorenkoKateryna <sydorenko.k.ih@gmail.com>
2023-02-25 14:53:27 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,7 @@ const categories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach((category) => {
-  console.log(`Category: ${category.querySelector("h2").textContent}`);
-  console.log(`Elements: ${category.querySelectorAll("li").length}`);
+  console.log(`Category: ${category.firstElementChild.textContent}`);
+  console.log(`Elements: ${category.lastElementChild.children.length}`);
+  
 });
